a9a32d4f4ce0bfb76b7b2f9f7178d1048ba1350f
dimitriy-ural <dimitriy.ural@gmail.com>
2026-04-08 18:43:36 +0500

    new script duble ava/photo

---
+++ script.js
@@ -0,0 +1,11 @@
+<!-- <script>
+    const avatarImg = document.querySelector('.avatar img');
+    
+    avatarImg.addEventListener('mouseenter', function() {
+        this.src = 'img/avatar-hover.jpg';
+    });
+    
+    avatarImg.addEventListener('mouseleave', function() {
+        this.src = 'img/avatar.jpeg';
+    });
+</script> -->
